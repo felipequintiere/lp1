07f7d5b6a6264e709018f8071d3e1245c819326e
Felipe Quintiere <felipequintiere@gmail.com>
2025-08-27 18:46:41 -0300

    atividade 4

---
+++ 04atividade/1.c
@@ -0,0 +1,35 @@
+/*
+ * 1.c
+ * Crie uma função eh_primo que recebe um inteiro n e retorna se ele é primo ou não.
+ * Exemplos:
+ * eh_primo(4) -> 0
+ * eh_primo(7) -> 1
+ * Dicas:
+ *
+ * A operação % calcula o resto entre dois valores.
+ * 11 % 4 = 3
+ * Em C, tipicamente usamos 1 e 0 para representar verdadeiro e falso, respectivamente.
+*/
+#include <stdio.h>
+#include <stdbool.h>
+
+bool eh_primo (int n)
+{
+	bool primo = true;
+
+	for (int i = 2; i < n; i++) 
+		if (n%i == 0) {primo = 0; break;}
+
+	return (primo != 0 ? 1 : 0); 
+}
+
+int main(void)
+{
+	int n;
+	printf("Insira um valor [n]: ");
+	scanf("%d",&n);
+
+	eh_primo(n) && n != 1 ? printf("%d é primo\n",n) : printf("%d não é primo\n",n);
+
+	return eh_primo(n);
+}
---
+++ 04atividade/2.c
@@ -0,0 +1,35 @@
+/*
+2.c
+Crie uma função todos_os_primos que recebe um inteiro max e exibe todos os primos entre 1 e max.
+Use a função eh_primo do exercício anterior sem alterá-la.
+*/
+#include <stdio.h>
+#include <stdbool.h>
+
+bool eh_primo (int n)
+{
+	bool primo = true;
+
+	for (int i = 2; i < n; i++) 
+		if (n%i == 0) {primo = 0; break;}
+
+	return (primo != 0 ? 1 : 0); 
+}
+
+int todos_os_primos (int n)
+{
+	for (int i = 2; i < n; i++)
+		eh_primo(i) ? printf("%d é primo\n",i) : i+0 ;	
+
+	return 0;
+}
+
+int main(void)
+{
+	int n;
+	printf("Insira um valor [n]: ");
+	scanf("%d",&n);
+
+	todos_os_primos(n);
+	return 0;
+}
