14b834d3adc4e00df5df57cbdf7fb20f5771ff95
Felipe Quintiere <felipequintiere@gmail.com>
2025-08-30 15:40:21 -0300

    teste

--- 02atividade/2.c
+++ 02atividade/2.c
@@ -10,7 +10,6 @@ int main(void)
 	printf("Escolha dois valores numéricos [n1 n2]: ");
 	scanf("%f%f", &n1, &n2);
 
-	// cascated if statements (p.80 - c programming a modern approach)
 	if (n1 > n2) printf("\nO maior valor é n1: %f\n",n1);
 	else
 		if (n2 > n1) printf("\nO maior valor é n2: %f\n",n2);
