5b013fc9772293fe45e8562d82d451e7ab57c25e
Felipe Quintiere <felipequintiere@gmail.com>
2025-09-19 12:32:53 -0300

    update

--- 02atividade/7.c
+++ 02atividade/7.c
@@ -9,9 +9,10 @@
 
 int main(void)
 {
-	int max, min, aux, vitoria, empate, linhas = 4, colunas = 2;
-	max = min = aux = 0;
-	int times[linhas][colunas];
+	int linhas = 4;
+	int times[linhas][2];
+	int vitoria, empate;
+	int max_index = 0, min_index = 0;
 
 	printf("\nInsira (na mesma linha) [id vitórias empates]\n");
 	for (int i = 0; i < linhas; i++)
@@ -21,43 +22,32 @@ int main(void)
 		times[i][1] = 3*vitoria + empate;
 	}
 
+
 	printf("\n%10s\t\t%10s\n","id","pontuação");
 	for (int i = 0; i < linhas; i++)
 	{
 		printf("%10.1d\t\t%10.1d\n",times[i][0],times[i][1]);
+
+		if (times[i][1] > times[max_index][1])
+			max_index = i;
+		if (times[i][1] < times[min_index][1])
+			min_index = i;
 	}
 
-	// pontuação máxima
-	for (int i = 0; i < linhas; i++)
-	{
-		if (times[i][1] > aux)
-		{
-			aux = times[i][1];
-			max = i;
-		}
-	}
 	printf("\nID(s) do(s) time(s) de maior pontuação: \n");
 	for (int i = 0; i < linhas; i++)
 	{
-		if (times[i][1] == times[max][1]) 
+		if (times[i][1] == times[max_index][1]) 
 			printf("%10.1d\n",times[i][0]);
 	}
 
-	// pontuação mínima
-	for (int i = 0; i < linhas; i++)
-	{
-		if (times[i][1] < aux)
-		{
-			aux = times[i][1];
-			min = i;
-		}
-	}
 	printf("\nID(s) do(s) time(s) de menor pontuação: \n");
 	for (int i = 0; i < linhas; i++)
 	{
-		if (times[i][1] == times[min][1]) 
+		if (times[i][1] == times[min_index][1]) 
 			printf("%10.1d\n",times[i][0]);
 	}
 
 	return 0;
 }
+
